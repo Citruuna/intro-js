726e871e2e397599be1066d317604be0a45c4327
Citruuna <55959622+Citruuna@users.noreply.github.com>
2020-01-13 13:19:48 +0200

    Added some test JS

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.warn("that was it");
+
+var a = 1;
+var b = 3;
+var c = a+b;
+
+var d = 4;
+var e = 4;
+var f = d + e
+
+if (a<b) { //do the following if a is lesser than b
+    console.log("a is lesser than b "); 
+    //do the following if a is greater than b
+}
+else {
+    console.log("a is greater than b")
+}
+
+
+if (a === 1 && b === 3) {
+console.log("a is 1 or b is 3")
+}
+else {
+    console.log("try again")
+}
+

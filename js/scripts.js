19b7951f35a326729230ea9577e18c4af6094ade
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-01-27 19:41:21 +0200

    Tada

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,21 @@ function calculate(order) {
         result = subtract(firstInput, secondInput);
     }
 
+    if (order === 'multiply') {
+      result = multiply(firstInput, secondInput);
+    } else if (order === 'divide')
+    result = divide(firstInput, secondInput);
+
+    if (order === 'pi') {
+      result = pi();
+    } else if (order === 'circumference')
+    result = circumference(firstInput, secondInput);
+
+    if (order === 'sqrt') {
+      result = sqrt(firstInput, secondInput);
+    } else if (order === 'pow')
+    result = pow(firstInput, secondInput);
+
     document.getElementById('final-result').innerText = result;
 }
 
@@ -17,13 +32,29 @@ function add(a, b){
 }
 
 function subtract(a, b) {
-
+return a - b;                     
 }
 
 function multiply(a, b) {
-
+return a * b;
 }
 
 function divide(a, b) {
+return a / b;
+}
 
+function pi() {
+return(Math.PI);             
+}
+
+function circumference(a, b) {
+return Math.PI*(a + b);             
+}
+
+function pow (a, b) {
+return(Math.pow(a, b));
+}
+  
+function sqrt(a, b) {
+return(Math.sqrt((a * a) + (b * b)));
 }

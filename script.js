11afe7f789d026c066308a167e3dbbbf70e0aa9a
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-01-31 12:24:08 +0200

    changes

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".product-image").css("background-color", "black");
+  $("#protein-bar").css("background-color", "blue");
+});
+function readyFn(jQuery) {
+    $(".branding").
+    
+$window.on("load", ready(readyFn)
+    
+
+}

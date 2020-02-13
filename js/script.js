f8058eac8e730bcfb74ce6ddf3cfc09a5c3ace26
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-02-13 14:37:30 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-var HOURHAND = document.getElementsById("hour");
-var MINUTEHAND = document.querySelector(".minute");
-var SECONDHAND = document.getElementsByName("second");
+function ticToc() {
+  let HOURHAND = document.getElementById("hour");
+  let MINUTEHAND = document.getElementById("minute");
+  let SECONDHAND = document.getElementById("second");
+  /** declaring the time */
+  const date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  /**declaring the positions */
+  let hrPosition = (hr * 360) / 12 + ((min / 60) * 360) / 12;
+  let minPosition = (min * 360) / 60 + (sec * 360) / 60 / 60;
+  let secPosition = (sec * 360) / 60;
 
-var date = new Date();
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
+  setInterval(function() {
+    secPosition += 6;
+    minPosition += sec / 60;
+    hrPosition += hr / 60;
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+  }, 1000);
+}

743d090fc27ebf95599cb7f683a25eb94230297e
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-02-13 10:37:59 +0200

    variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+var HOURHAND = document.getElementsById("hour");
+var MINUTEHAND = document.querySelector(".minute");
+var SECONDHAND = document.getElementsByName("second");
+
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
